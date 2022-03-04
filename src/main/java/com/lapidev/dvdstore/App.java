c1eaffcc39a4439e0bb2424293b7d06acfcafc0a
Jossic <jossic.lapierre@gmail.com>
2022-03-04 14:52:48 +0100

    First exercise done

--- src/main/java/com/lapidev/dvdstore/App.java
+++ src/main/java/com/lapidev/dvdstore/App.java
@@ -1,5 +1,10 @@
 package com.lapidev.dvdstore;
 
+import com.lapidev.dvdstore.entity.Movie;
+import com.lapidev.dvdstore.service.MovieService;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner sc = new Scanner(System.in);
+        System.out.println( "Saisissez un titre ?" );
+        String title = sc.nextLine();
+
+        System.out.println( "Saisissez un genre ?" );
+        String genre = sc.nextLine();
+
+        Movie newMovie = new Movie();
+        newMovie.setTitle(title);
+        newMovie.setGenre(genre);
+
+        MovieService movieService = new MovieService();
+        movieService.registerMovie(newMovie);
     }
 }
--- src/main/java/com/lapidev/dvdstore/service/MovieService.java
+++ src/main/java/com/lapidev/dvdstore/service/MovieService.java
@@ -1,8 +1,13 @@
 package com.lapidev.dvdstore.service;
 
+import com.lapidev.dvdstore.entity.Movie;
+import com.lapidev.dvdstore.repository.MovieRepository;
+
 public class MovieService {
 
-    public void registerMovie(Movie movie) {
+    private MovieRepository movieRepository;
 
+    public void registerMovie(Movie movie) {
+    movieRepository.add(movie);
     }
 }
